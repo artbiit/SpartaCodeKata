dcc394cf9f78e09079e0fc35a2774093d4dcb449
ljs <artbiit@naver.com>
2024-08-26 11:58:04 +0900

    나이 출력

--- solution.js
+++ solution.js
@@ -1,3 +1,4 @@
-function solution(age) {
-  return 2023 - age ;
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
 }
+//https://school.programmers.co.kr/learn/courses/30/lessons/120807
