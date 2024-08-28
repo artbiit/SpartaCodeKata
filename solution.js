918165c67f152a17c357fce2009a22387dfbbfb9
ljs <artbiit@naver.com>
2024-08-28 09:34:34 +0900

    두 수의 나눗셈

--- solution.js
+++ solution.js
@@ -1,2 +1,4 @@
-const solution = (num1, num2)  => num1 + num2;
-https://school.programmers.co.kr/learn/courses/30/lessons/120802
+function solution(num1, num2) {
+  return (num1 / num2 * 1000) | 0;
+}
+https://school.programmers.co.kr/learn/courses/30/lessons/120806
