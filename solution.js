2e344f4236c38c2aa0168573b9f5553cc2fc83fa
ljs <artbiit@naver.com>
2024-08-28 10:04:18 +0900

    짝수의 합

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
-function solution(angle) {
-  return ((angle / 90 ) | 0) + (angle > 90) + 1;
+function solution(n) {
+  n = n & (Number.MAX_SAFE_INTEGER - 1); //짝수로 변경
+  // 2 * (k * (k + 1)) / 2 = k * (k + 1)
+  n >>= 1;  
+ return n * (n + 1);
 }
-///https://school.programmers.co.kr/learn/courses/30/lessons/120829
-//js에서 Number 와 bool 타입의 연산은 bool을 true - 1, false - 0 으로 변환함
+//https://school.programmers.co.kr/learn/courses/30/lessons/120831
