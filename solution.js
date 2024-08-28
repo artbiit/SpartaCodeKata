424052a1ece18404aa46bbec955e2a7993e0bd5a
ljs <artbiit@naver.com>
2024-08-28 10:20:24 +0900

    평균 구하기

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
   return numbers.reduce((acc, currentValue) => acc + currentValue, 0) / numbers.length;
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/120817
+//https://school.programmers.co.kr/learn/courses/30/lessons/12944
