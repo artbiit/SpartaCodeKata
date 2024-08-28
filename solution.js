1e901e87123cb589cd609336b718a06f66e8e432
ljs <artbiit@naver.com>
2024-08-28 10:53:11 +0900

    약수의 합

--- solution.js
+++ solution.js
@@ -1,11 +1,15 @@
-function solution(n)
-{
-    let sum = 0;
-    while (n > 0) {
-        sum += n % 10; 
-        n = (n / 10) | 0; 
+function solution(n) {
+  let sum = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      sum += i;
+      let pairDivisor = n / i;
+      if (i !== pairDivisor) {
+        sum += pairDivisor;
+      }
     }
-    return sum;
+  }
+  return sum;
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12931
+//https://school.programmers.co.kr/learn/courses/30/lessons/12928
