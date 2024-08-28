22744e0be405040c7efe478678ddffe889a864d4
ljs <artbiit@naver.com>
2024-08-28 09:49:20 +0900

    각도기

--- solution.js
+++ solution.js
@@ -1,4 +1,5 @@
-function solution(num1, num2) {
-  return (num1 / num2 * 1000) | 0;
+function solution(angle) {
+  return ((angle / 90 ) | 0) + (angle > 90) + 1;
 }
-https://school.programmers.co.kr/learn/courses/30/lessons/120806
+///https://school.programmers.co.kr/learn/courses/30/lessons/120829
+//js에서 Number 와 bool 타입의 연산은 bool을 true - 1, false - 0 으로 변환함
