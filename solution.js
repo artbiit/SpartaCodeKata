9093a212fe06074949c5867f873a49c8319373d9
ljs <artbiit@naver.com>
2024-08-28 10:12:31 +0900

    배열의 평균값

--- solution.js
+++ solution.js
@@ -1,7 +1,4 @@
-function solution(n) {
-  n = n & (Number.MAX_SAFE_INTEGER - 1); //짝수로 변경
-  // 2 * (k * (k + 1)) / 2 = k * (k + 1)
-  n >>= 1;  
- return n * (n + 1);
+function solution(numbers) {
+  return numbers.reduce((acc, currentValue) => acc + currentValue, 0) / numbers.length;
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/120831
+//https://school.programmers.co.kr/learn/courses/30/lessons/120817
