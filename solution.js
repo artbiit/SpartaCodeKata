0c3a6be84d826b320ff03ed7b95a60dbe13fb6f2
ljs <artbiit@naver.com>
2024-08-28 09:31:21 +0900

    두 수의 합

--- solution.js
+++ solution.js
@@ -1,4 +1,2 @@
-function solution(num1, num2) {
-  return num1 === num2 ? 1 : -1;
-}
-//https://school.programmers.co.kr/learn/courses/30/lessons/120807
+const solution = (num1, num2)  => num1 + num2;
+https://school.programmers.co.kr/learn/courses/30/lessons/120802
