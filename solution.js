1bb89c34857d82260f773e2a6967528b39f7b37b
ljs <artbiit@naver.com>
2024-08-29 09:57:53 +0900

    문자열을 정수로 바꾸기

--- solution.js
+++ solution.js
@@ -1,11 +1,25 @@
-function solution(n) {
-  var answer = [];
-  
-  while(n > 0){
-      answer.push((n % 10) | 0);
-      n = (n / 10) | 0;
-  }
-  return answer;
+function solution(s) {
+  let result = 0, index = 0, decimalFactor = 0, sign = 1, character;
+
+ while ((character = s.charCodeAt(index++)) === 32); // 공백 무시
+
+ if (character === 43 || character === 45) { // '+' 또는 '-' 확인
+     sign = 1 - 2 * (character === 45);
+     character = s.charCodeAt(index++);
+ }
+
+ while (character >= 48 && character <= 57 || character === 46) {
+     if (character === 46) {
+         if (decimalFactor) break; // 두 번째 소수점이 나오면 NaN
+         decimalFactor = 1;
+     } else {
+         result = result * 10 + (character - 48);
+         decimalFactor && (decimalFactor *= 10);
+     }
+     character = s.charCodeAt(index++);
+ }
+
+ return index > 1 && !character ? sign * result / (decimalFactor || 1) : NaN;
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12932
+//https://school.programmers.co.kr/learn/courses/30/lessons/12925
