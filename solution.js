ee00d57c9309e3e1d8e2c6857d274f942b707857
ljs <artbiit@naver.com>
2024-08-29 11:28:18 +0900

    핸드폰 번호 가리기

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
-function solution(absolutes, signs) {
-  return absolutes.reduce((sum, value, i) => sum + value * (signs[i] ? 1 : -1), 0);
+function solution(phone_number) {
+  return '*'.repeat(phone_number.length-4) + phone_number.slice(-4) ;
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/76501
+//https://school.programmers.co.kr/learn/courses/30/lessons/12948
