8a68261f72890b94247067323609c265a62b87c6
ljs <artbiit@naver.com>
2024-08-29 11:11:48 +0900

    김서방 찾기

--- solution.js
+++ solution.js
@@ -1,14 +1,5 @@
-function solution(num) {
-  let i = 0;
-  while( i < 500 && num !== 1){
-      if((num & 1) === 0){
-          num /= 2;
-      }else{
-          num = num * 3 +1; 
-      }
-      i++;
-  }
-  
-  return (i >= 500 ? -1 : i);
+function solution(seoul) {
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12919
