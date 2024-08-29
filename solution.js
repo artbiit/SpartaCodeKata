7c5cc6da8c9bb8780ea6a9a7df7f4bfb32bec944
ljs <artbiit@naver.com>
2024-08-29 09:25:31 +0900

    나머지가 1이 되는 수 중 제일 작은 수

--- solution.js
+++ solution.js
@@ -1,15 +1,10 @@
 function solution(n) {
-  let sum = 0;
-  for (let i = 1; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      sum += i;
-      let pairDivisor = n / i;
-      if (i !== pairDivisor) {
-        sum += pairDivisor;
-      }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 1) {
+      return i;
     }
   }
-  return sum;
+    return n-1;
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12928
+//https://school.programmers.co.kr/learn/courses/30/lessons/87389
