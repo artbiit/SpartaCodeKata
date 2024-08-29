ed16861d29f1e76f95f82d274aa147e205f152c8
ljs <artbiit@naver.com>
2024-08-29 11:17:18 +0900

    나누어 떨어지는 숫자 배열

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
-function solution(seoul) {
-  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+function solution(arr, divisor) {
+  arr = arr.filter(num => num % divisor === 0).sort( (a,b) => a-b);
+  return arr.length === 0 ? [-1] : arr;
 }
-
-//https://school.programmers.co.kr/learn/courses/30/lessons/12919
+//https://school.programmers.co.kr/learn/courses/30/lessons/12910
