ef70bb7a08c11ae2d19e344b95885817c2f52787
ljs <artbiit@naver.com>
2024-08-29 10:10:41 +0900

    정수 제곱근 판별

--- solution.js
+++ solution.js
@@ -1,25 +1,11 @@
-function solution(s) {
-  let result = 0, index = 0, decimalFactor = 0, sign = 1, character;
+function solution(n) {
+  let sqrt = Math.sqrt(n) | 0;
 
- while ((character = s.charCodeAt(index++)) === 32); // 공백 무시
-
- if (character === 43 || character === 45) { // '+' 또는 '-' 확인
-     sign = 1 - 2 * (character === 45);
-     character = s.charCodeAt(index++);
- }
-
- while (character >= 48 && character <= 57 || character === 46) {
-     if (character === 46) {
-         if (decimalFactor) break; // 두 번째 소수점이 나오면 NaN
-         decimalFactor = 1;
-     } else {
-         result = result * 10 + (character - 48);
-         decimalFactor && (decimalFactor *= 10);
-     }
-     character = s.charCodeAt(index++);
- }
-
- return index > 1 && !character ? sign * result / (decimalFactor || 1) : NaN;
+  if(sqrt * (sqrt++) === n){
+      return sqrt * sqrt;
+  }else{
+      return -1;
+  }
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12925
+//https://school.programmers.co.kr/learn/courses/30/lessons/12934
