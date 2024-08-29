4a8e871a0e538801f07de1911975458132720239
ljs <artbiit@naver.com>
2024-08-29 10:33:02 +0900

    하샤드 수

--- solution.js
+++ solution.js
@@ -1,5 +1,12 @@
-function solution(n) {
-  return Number(n.toString().split('').sort((a, b) => b - a).join(''));
+function solution(x) {
+  let sum = 0;
+  let tmp = x;
+  while(tmp > 0){
+      sum += tmp % 10;
+      tmp = (tmp / 10) | 0;
+  }
+  
+  return !(x % sum)
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12933
+//https://school.programmers.co.kr/learn/courses/30/lessons/12947
