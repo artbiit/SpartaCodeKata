12f1f001deadee4d361a134641b80efd5f24c969
ljs <artbiit@naver.com>
2024-08-29 12:15:22 +0900

    가운데 글자 가져오기

--- solution.js
+++ solution.js
@@ -1,8 +1,6 @@
-function solution(arr) {
-  if(arr.length === 1){
-      return arr[0] === 10 ? [-1] : arr;
-  }
-   arr.splice(arr.indexOf(Math.min(... arr)), 1);
-  return  arr;
+function solution(s) {
+  const len = s.length;
+  const mid = len >> 1;
+  return s.slice(mid - (1 - len & 1), mid + 1);
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/12935
+//https://school.programmers.co.kr/learn/courses/30/lessons/12903
