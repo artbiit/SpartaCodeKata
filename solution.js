20463cb6684dbb8039b7d3becb03ad9385b9c0ae
ljs <artbiit@naver.com>
2024-09-13 09:50:46 +0900

    약수의 개수와 덧셈

--- solution.js
+++ solution.js
@@ -1,4 +1,25 @@
-function solution(a, b) {
-  return a.reduce((acc, _, idx) => acc + a[idx] * b[idx], 0);
+function measureCount(n) {
+  let count = 0;
+  const sqrtN = Math.sqrt(n);
+
+  for (let i = 1; i <= sqrtN; i++) {
+    if (n % i === 0) {
+      count += 2;
+    }
+  }
+
+  if (sqrtN % 1 === 0) {
+    count--;
+  }
+
+  return count;
+}
+
+function solution(left, right) {
+  let agg = 0;
+  for (let i = left; i <= right; i++) {
+    agg += (measureCount(i) & 1) === 0 ? i : -i;
+  }
+  return agg;
 }
 //https://school.programmers.co.kr/learn/courses/30/lessons/77884
