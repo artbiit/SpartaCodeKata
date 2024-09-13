898ab8f512355a971bcab7d55f84af84b5661e72
ljs <artbiit@naver.com>
2024-09-13 10:05:00 +0900

    문자열 내림차순 배치하기

--- solution.js
+++ solution.js
@@ -1,25 +1,4 @@
-function measureCount(n) {
-  let count = 0;
-  const sqrtN = Math.sqrt(n);
-
-  for (let i = 1; i <= sqrtN; i++) {
-    if (n % i === 0) {
-      count += 2;
-    }
-  }
-
-  if (sqrtN % 1 === 0) {
-    count--;
-  }
-
-  return count;
+function solution(s) {
+  return [...s].sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0)).join("");
 }
-
-function solution(left, right) {
-  let agg = 0;
-  for (let i = left; i <= right; i++) {
-    agg += (measureCount(i) & 1) === 0 ? i : -i;
-  }
-  return agg;
-}
-//https://school.programmers.co.kr/learn/courses/30/lessons/77884
+//https://school.programmers.co.kr/learn/courses/30/lessons/12917
