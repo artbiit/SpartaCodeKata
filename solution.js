99e4f3af37249056099790f48bd85d2f80c415d8
ljs <artbiit@naver.com>
2024-09-13 10:40:08 +0900

    문자열 다루기 기본

--- solution.js
+++ solution.js
@@ -1,5 +1,13 @@
-function solution(price, money, count) {
-  price *= ((count * (count + 1)) / 2) | 0;
-  return money < price ? price - money : 0;
+function solution(s) {
+  const len = s.length;
+  if (len !== 4 && len !== 6) return false;
+
+  for (let i = 0; i < len; i++) {
+    const code = s.charCodeAt(i);
+    if (code < 48 || code > 57) return false;
+  }
+
+  return true;
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/82612
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12918
