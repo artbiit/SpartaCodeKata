a791eb891a362484f69aa8f509b56a5ed492539c
ljs <artbiit@naver.com>
2024-09-13 09:27:12 +0900

    약수의 개수와 덧셈 브랜치

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
 function solution(a, b) {
   return a.reduce((acc, _, idx) => acc + a[idx] * b[idx], 0);
 }
-//https://school.programmers.co.kr/learn/courses/30/lessons/70128?language=javascript
+//https://school.programmers.co.kr/learn/courses/30/lessons/77884
