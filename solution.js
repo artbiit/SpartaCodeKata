1aad33c85b4677a30e36bf89f5f175e05f3f6183
ljs <artbiit@naver.com>
2024-09-19 09:53:32 +0900

    최대공약수와 최소공배수

--- solution.js
+++ solution.js
@@ -1,15 +1,14 @@
-function solution(n) {
-  let sum = 0;
-  for (let i = 1; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      sum += i;
-      let pairDivisor = n / i;
-      if (i !== pairDivisor) {
-        sum += pairDivisor;
-      }
-    }
+function gcd(a, b) {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
   }
-  return sum;
+  return a;
+}
+function solution(n, m) {
+  const __gcd = gcd(n, m);
+  return [__gcd, (n * m) / __gcd];
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12928
+//https://school.programmers.co.kr/learn/courses/30/lessons/12940
