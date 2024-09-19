1225df29c927afa5658da91ce07549f436352701
ljs <artbiit@naver.com>
2024-09-19 10:03:38 +0900

    3진법 뒤집기

--- solution.js
+++ solution.js
@@ -1,14 +1,10 @@
-function gcd(a, b) {
-  while (b !== 0) {
-    const temp = b;
-    b = a % b;
-    a = temp;
+function solution(n) {
+  let arr = [];
+  let i = 1;
+  while (n > 0) {
+    arr.unshift(n % 3);
+    n = Math.floor(n / 3);
   }
-  return a;
+  return arr.reduce((acc, cur, idx) => acc + cur * 3 ** idx, 0);
 }
-function solution(n, m) {
-  const __gcd = gcd(n, m);
-  return [__gcd, (n * m) / __gcd];
-}
-
-//https://school.programmers.co.kr/learn/courses/30/lessons/12940
+//https://school.programmers.co.kr/learn/courses/30/lessons/68935
