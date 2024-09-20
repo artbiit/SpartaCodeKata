7787ff5e5553c8205fd030c7ee5bb1cc0137b0a0
ljs <artbiit@naver.com>
2024-09-20 23:42:29 +0900

    k번째수

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
-const solution = (strings, n) =>
-  strings.sort((a, b) => {
-    const compare = a.charCodeAt(n) - b.charCodeAt(n);
-    return compare ? compare : a.localeCompare(b);
-  });
-//https://school.programmers.co.kr/learn/courses/30/lessons/12915
+const solution = (array, commands) => {
+  var answer = [];
+  for (let c = 0; c < commands.length; c++) {
+    const cmd = commands[c];
+    answer.push(
+      array.slice(cmd[0] - 1, cmd[1]).sort((a, b) => a - b)[cmd[2] - 1]
+    );
+  }
+  return answer;
+};
+//https://school.programmers.co.kr/learn/courses/30/lessons/42748
