98bee1de3021d6d08472a62b384ff813603286fb
ljs <artbiit@naver.com>
2024-09-20 10:16:08 +0900

    시저 암호

--- solution.js
+++ solution.js
@@ -1,16 +1,19 @@
-function solution(sizes) {
-  let w = 0;
-  let h = 0;
+function solution(s, n) {
+  return [...s]
+    .map((value) => {
+      if (value === " ") return value; // 공백 처리
 
-  for (let i = 0; i < sizes.length; i++) {
-    const width = Math.max(sizes[i][0], sizes[i][1]);
-    const height = Math.min(sizes[i][0], sizes[i][1]);
+      let charCode = value.charCodeAt(0);
 
-    w = Math.max(w, width);
-    h = Math.max(h, height);
-  }
+      if (charCode >= 65 && charCode <= 90) {
+        charCode = ((charCode - 65 + n) % 26) + 65;
+      } else if (charCode >= 97 && charCode <= 122) {
+        charCode = ((charCode - 97 + n) % 26) + 97;
+      }
 
-  return w * h;
+      return String.fromCharCode(charCode);
+    })
+    .join("");
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/86491
+//https://school.programmers.co.kr/learn/courses/30/lessons/12926
