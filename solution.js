f12fd3e20ed786016359293cd63918f5f8c73997
ljs <artbiit@naver.com>
2024-09-27 10:10:55 +0900

    카드 뭉치

--- solution.js
+++ solution.js
@@ -1,23 +1,18 @@
-const dayOfTheWeek = ["SAT", "SUN", "MON", "TUE", "WED", "THU", "FRI"];
-function solution(a, b) {
-  let year = 2016;
-  if (a < 3) {
-    a += 12;
-    year -= 1;
-  }
-  let K = year % 100;
-  let J = Math.floor(year / 100);
+function solution(cards1, cards2, goal) {
+  let firstCard = 0;
+  let secondCard = 0;
 
-  let index =
-    (b +
-      Math.floor((13 * (a + 1)) / 5) +
-      K +
-      Math.floor(K / 4) +
-      Math.floor(J / 4) -
-      2 * J) %
-    7;
-  return dayOfTheWeek[index < 0 ? index + 7 : index];
+  for (const current of goal) {
+    if (current === cards1[firstCard]) {
+      firstCard++;
+    } else if (current === cards2[secondCard]) {
+      secondCard++;
+    } else {
+      return "No";
+    }
+  }
+
+  return "Yes";
 }
 
-console.log(solution(5, 24));
-//https://school.programmers.co.kr/learn/courses/30/lessons/138477
+//https://school.programmers.co.kr/learn/courses/30/lessons/159994
