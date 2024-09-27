e07b5110efeaec904d63ef0df21bf47682d287d9
ljs <artbiit@naver.com>
2024-09-27 09:37:02 +0900

    명예의 전당 (1)

--- solution.js
+++ solution.js
@@ -1,12 +1,17 @@
-function solution(a, b, n) {
-  let result = 0;
-  while (n >= a) {
-    const currentCount = ((n / a) | 0) * b;
-    result += currentCount;
-    n = (n % a) + currentCount;
-  }
-  return result;
-}
+function solution(k, score) {
+  var map = [];
+  let min = [];
+  for (let i = 0; i < score.length; i++) {
+    const currentScore = score[i];
+    if (min.length < k) {
+      min.push(currentScore);
+    } else if (min[0] <= currentScore) {
+      min[0] = currentScore;
+    }
 
-console.log(solution(2, 1, 20));
-//https://school.programmers.co.kr/learn/courses/30/lessons/134240
+    min.sort((a, b) => a - b);
+    map.push(min[0]);
+  }
+  return map;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/138477
