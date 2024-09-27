8ea156d2bd76b68f779f1b8961aef307a345841e
ljs <artbiit@naver.com>
2024-09-27 09:58:08 +0900

    2016년

--- solution.js
+++ solution.js
@@ -1,17 +1,23 @@
-function solution(k, score) {
-  var map = [];
-  let min = [];
-  for (let i = 0; i < score.length; i++) {
-    const currentScore = score[i];
-    if (min.length < k) {
-      min.push(currentScore);
-    } else if (min[0] <= currentScore) {
-      min[0] = currentScore;
-    }
-
-    min.sort((a, b) => a - b);
-    map.push(min[0]);
+const dayOfTheWeek = ["SAT", "SUN", "MON", "TUE", "WED", "THU", "FRI"];
+function solution(a, b) {
+  let year = 2016;
+  if (a < 3) {
+    a += 12;
+    year -= 1;
   }
-  return map;
+  let K = year % 100;
+  let J = Math.floor(year / 100);
+
+  let index =
+    (b +
+      Math.floor((13 * (a + 1)) / 5) +
+      K +
+      Math.floor(K / 4) +
+      Math.floor(J / 4) -
+      2 * J) %
+    7;
+  return dayOfTheWeek[index < 0 ? index + 7 : index];
 }
+
+console.log(solution(5, 24));
 //https://school.programmers.co.kr/learn/courses/30/lessons/138477
