c42ec38e776a0034fe0752f67f61f443728ab867
ljs <artbiit@naver.com>
2024-10-28 09:21:18 +0900

    에어컨

--- solution.js
+++ solution.js
@@ -1,37 +1,45 @@
-function getModValue(row, row_num) {
-  return row.reduce((acc, cur) => acc + (cur % row_num), 0);
-}
+function solution(temperature, t1, t2, a, b, onboard) {
+  const temp = temperature > t2 ? t1 - (temperature - t2) : temperature;
+  const INF = Number.MAX_SAFE_INTEGER;
 
-function solution(data, col, row_begin, row_end) {
-  data.sort((a, b) => {
-    const aCol = a[col - 1];
-    const bCol = b[col - 1];
+  t1 -= temp;
+  t2 -= temp;
+  const adjustedTemp = 0;
 
-    if (aCol === bCol) {
-      return b[0] - a[0];
+  const dp = Array.from({ length: onboard.length }, () =>
+    Array(t2 + 2).fill(INF)
+  );
+  dp[0][adjustedTemp] = 0;
+
+  for (let i = 1; i < onboard.length; i++) {
+    for (let j = 0; j <= t2 + 1; j++) {
+      if (onboard[i] === 1 && (j < t1 || j > t2)) continue;
+      let min = INF;
+
+      if (j === 0) {
+        min = Math.min(min, dp[i - 1][j]);
+        if (j + 1 <= t2 + 1) min = Math.min(min, dp[i - 1][j + 1]);
+      } else {
+        if (j - 1 >= 0) min = Math.min(min, dp[i - 1][j - 1] + a);
+        min = Math.min(min, dp[i - 1][j] + b);
+        if (j + 1 <= t2 + 1) min = Math.min(min, dp[i - 1][j + 1]);
+      }
+
+      dp[i][j] = min;
     }
+  }
 
-    return aCol - bCol;
-  });
-  let result = getModValue(data[row_begin - 1], row_begin);
-  for (let i = row_begin; i < row_end; i++) {
-    result ^= getModValue(data[i], i + 1);
+  let result = INF;
+  for (let j = 0; j <= t2 + 1; j++) {
+    result = Math.min(result, dp[onboard.length - 1][j]);
   }
 
   return result;
 }
 
-console.log(
-  solution(
-    [
-      [2, 2, 6],
-      [1, 5, 10],
-      [4, 2, 9],
-      [3, 8, 3],
-    ],
-    2,
-    2,
-    3
-  )
-);
-//https://school.programmers.co.kr/learn/courses/30/lessons/147354
+console.log(solution(28, 18, 26, 10, 8, [0, 0, 1, 1, 1, 1, 1])); // 40
+console.log(solution(-10, -5, 5, 5, 1, [0, 0, 0, 0, 0, 1, 0])); // 25
+console.log(solution(11, 8, 10, 10, 1, [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1])); // 20
+console.log(solution(11, 8, 10, 10, 100, [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1])); // 60
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/214289
